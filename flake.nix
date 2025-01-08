21b3e924989cceb44bf2f756ef047db0b03668ea
Angel Ortiz <aandresortiz@gmail.com>
2025-01-08 01:36:30 -0500

    add base nix config

---
+++ flake.nix
@@ -0,0 +1,151 @@
+{
+  description = "Nix-darwin system flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin = {
+            url = "github:LnL7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    };
+
+    outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }: 
+    let
+        configuration = { pkgs, ... }: {
+            # List packages installed in system profile. To search by name, run:
+            # $ nix-env -qaP | grep wget
+            environment.systemPackages = [
+            ];
+
+            homebrew = {
+                enable = true;
+                brews = [
+                    "mas"
+                ];
+                casks = [
+                    "visual-studio-code"
+                    "keka"
+                    "pycharm"
+                    "wezterm"
+                    "google-chrome"
+                    "elmedia-player"
+                    "gimp"
+                    "inkscape"
+                    "beekeeper-studio"
+                    "bruno"
+                    "discord"
+                    "telegram"
+                    "fleet" 
+                ];
+
+                masApps = {
+                    "SparkMail" = 1176895641; 
+                };
+                onActivation.cleanup = "zap";
+                onActivation.autoUpdate = true;
+                onActivation.upgrade = true;
+            };
+
+            fonts.packages = [
+                pkgs.nerd-fonts.fira-code
+            ];
+
+            services.nix-daemon.enable = true;
+            nix.settings.experimental-features = "nix-command flakes";
+            # Enable alternative shell support in nix-darwin.
+            # programs.zsh.enable = true;
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+            # Used for backwards compatibility, please read the changelog before changing.
+            # $ darwin-rebuild changelog
+            system.stateVersion = 5;
+            system.defaults = {
+                dock = {
+                    autohide = true;
+                    tilesize = 32;
+                    largesize = 128;
+                    show-recents = false;
+                    minimize-to-application = true;
+                    mineffect = "genie";
+                    magnification = true;
+                    persistent-apps = [
+                        "/Applications/WezTerm.app"
+                        "/Applications/Spark.app"
+                        "/System/Cryptexes/App/System/Applications/Safari.app"
+                        "/Applications/Google Chrome.app"
+                        "/Applications/Pycharm.app"
+                        "/Applications/Visual Studio Code.app"
+                        "/System/Applications/Photos.app"
+                        "/System/Applications/Calendar.app"
+                        "/System/Applications/Contacts.app"
+                        "/System/Applications/Reminders.app"
+                        "/System/Applications/Notes.app"
+                        "/System/Applications/Music.app"
+                        "/System/Applications/App Store.app"
+                        "/System/Applications/System Settings.app"
+                    ];
+                };
+                controlcenter = {
+                    AirDrop = true;
+                    BatteryShowPercentage = true;
+                    Bluetooth = true;
+                    NowPlaying = true;
+                    Sound = true;
+                };
+                trackpad = {
+                    Clicking = true;
+                    Dragging = true;
+                };
+                finder = {
+                    ShowPathbar = true;
+                    ShowStatusBar = true;
+                    AppleShowAllFiles = true;
+                };
+                menuExtraClock = {
+                    Show24Hour = true;
+                    ShowSeconds = true;
+                    ShowAMPM = false;
+                };
+                NSGlobalDomain = {
+                    "com.apple.mouse.tapBehavior" = 1;
+                    _HIHideMenuBar = false;
+                    AppleInterfaceStyle = "Dark";
+                    AppleMeasurementUnits = "Centimeters";
+                };
+                SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+            };
+
+            security.pam.enableSudoTouchIdAuth = true;
+
+            # The platform the configuration will be used on.
+            nixpkgs.hostPlatform = "aarch64-darwin";
+            nixpkgs.config.allowUnfree = true;
+        };
+    in {
+        # Build darwin flake using:
+        # $ darwin-rebuild build --flake .#simple
+        darwinConfigurations."antares" = nix-darwin.lib.darwinSystem {
+            modules = [
+                configuration
+                nix-homebrew.darwinModules.nix-homebrew {
+                    nix-homebrew = {
+                        enable = true;
+                        user = "coder";
+                    };
+                }
+                home-manager.darwinModules.home-manager {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.coder = import ./home.nix;
+                    };
+                    users.users.coder.home = "/Users/coder";
+                }
+            ];
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }: {
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  # home.username = "coder";
+    home = {
+        enableNixpkgsReleaseCheck = true;
+        stateVersion = "24.11";
+        shellAliases = {
+            ll = "eza -lF --color-scale --no-user --no-time --no-permissions --group-directories-first --icons -a";
+            ls = "eza -lF --group-directories-first --icons -a";
+            ".." = "cd ..";
+        };
+        packages = with pkgs; [
+            eza
+            neovim
+            starship
+            htop
+        ];
+        file = {
+            ".zshrc".source = ./dotfiles/zshrc/zshrc;
+            ".wezterm.lua".source = ./dotfiles/wezterm/wezterm.lua;
+        };
+    };
+    # Let Home Manager install and manage itself.
+    programs = {
+        home-manager = {
+            enable = true;
+            path = "$HOME/.config/nix-darwin/nixpkgs/";
+        };
+        git = {
+            enable = true;
+            userName = "Angel Ortiz";
+            userEmail = "aandresortiz@gmail.com";
+            ignores = [ "**/.idea/" "**/.vscode/settings.json" "**/.direnv/" "**/.DS_Store" ];
+        };
+        zsh = {
+            enable = true;
+            autocd = true;
+            enableCompletion = true;
+            autosuggestion.enable = true;
+            syntaxHighlighting.enable = true;
+        };
+    };
+}
